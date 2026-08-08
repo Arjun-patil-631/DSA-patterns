ffe978fce375dce0782923d8e7525b5010adac1b
Arjun-patil-631 <5631arjunpatil@gmail.com>
2026-08-08 17:21:50 +0530

    Initial commit

---
+++ Two-pointers/two-pointer.c
@@ -0,0 +1,12 @@
+//1.Remove duplicates from a sorted array
+int removeDuplicates(int* nums, int numsSize) {
+    int i=0;
+    for(int j=0; j<numsSize; j++){
+        if(nums[i]!=nums[j]){
+            i++;
+            nums[i]=nums[j];
+        }
+    }
+    return i+1;
+}
+
